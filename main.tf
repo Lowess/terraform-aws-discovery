87f5cb773b6161f9cd1bd71a9b4a22ac87d7eda3
Florian Dambrine <florian@gumgum.com>
2023-11-14 16:10:30 +0100

    Fix deprecated data.aws_subnet_ids

--- main.tf
+++ main.tf
@@ -21,9 +21,11 @@ data "aws_availability_zones" "azs" {
 ### Subnets
 
 # VPC private tier subnets
-data "aws_subnet_ids" "vpc_private_subnets" {
-  vpc_id = data.aws_vpc.vpc.id
-
+data "aws_subnets" "vpc_private_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
   filter {
     name = "tag:${var.discovery_tag_key}"
     values = [
@@ -34,14 +36,16 @@ data "aws_subnet_ids" "vpc_private_subnets" {
 
 # VPC private tier subnets azs information
 data "aws_subnet" "vpc_private_subnets_info" {
-  for_each = data.aws_subnet_ids.vpc_private_subnets.ids
+  for_each = data.aws_subnets.vpc_private_subnets.ids
   id       = each.value
 }
 
 # VPC public tier subnets
-data "aws_subnet_ids" "vpc_public_subnets" {
-  vpc_id = data.aws_vpc.vpc.id
-
+data "aws_subnets" "vpc_public_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
   filter {
     name = "tag:${var.discovery_tag_key}"
     values = [
@@ -52,7 +56,7 @@ data "aws_subnet_ids" "vpc_public_subnets" {
 
 # VPC public tier subnets azs information
 data "aws_subnet" "vpc_public_subnets_info" {
-  for_each = data.aws_subnet_ids.vpc_public_subnets.ids
+  for_each = data.aws_subnets.vpc_public_subnets.ids
   id       = each.value
 }
 
@@ -70,7 +74,7 @@ data "aws_subnet" "vpc_subnets" {
 
 # VPC private route table information
 data "aws_route_table" "vpc_private_route_table_info" {
-  for_each  = data.aws_subnet_ids.vpc_private_subnets.ids
+  for_each  = data.aws_subnets.vpc_private_subnets.ids
   subnet_id = each.value
 }
 
